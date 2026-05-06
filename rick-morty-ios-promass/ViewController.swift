4522f00d751d95966cf627e8c4f006b2e4286bfd
sebsverastegui <sebas.paps.12@gmail.com>
2026-05-05 19:16:02 -0600

    Initial Commit

---
+++ rick-morty-ios-promass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick-morty-ios-promass
+//
+//  Created by seb's on 5/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ rick-morty-ios-promassUITests/rick_morty_ios_promassUITests.swift
@@ -0,0 +1,41 @@
+//
+//  rick_morty_ios_promassUITests.swift
+//  rick-morty-ios-promassUITests
+//
+//  Created by seb's on 5/5/26.
+//
+
+import XCTest
+
+final class rick_morty_ios_promassUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    @MainActor
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    @MainActor
+    func testLaunchPerformance() throws {
+        // This measures how long it takes to launch your application.
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
+        }
+    }
+}
